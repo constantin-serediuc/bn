2d4a3845a2f3df2519dcbcbb9552eeeee4efcce3
constantinserediuc <serediuc.constantin@info.uaic.ro>
2020-01-15 22:22:40 +0200

    add step for Env and reward logic

--- rl/net_env.py
+++ rl/net_env.py
@@ -11,6 +11,22 @@ import networkx as nx
 
 from rl.parameters import MAX_STEPS
 
+REMOVE_WITH_GREATER_LLOG = 1
+
+REMOVE_BUT_SMALLER_LLOG = 0
+
+REMOVE_BUT_EDGE_NOT_EXISTS = 0
+
+DRAW_WITH_GREATER_LLOG = 1
+
+DRAW_BUT_SMALLER_LLOG = 0
+
+DRAW_BUT_ALREADY_EXISTS = 0
+
+ONLY_MOVE = 0
+
+SAME_NODE = -2
+
 
 class NetEnv(gym.Env):
     def __init__(self, data: pd.DataFrame):
@@ -50,35 +66,56 @@ class NetEnv(gym.Env):
         self.current_step += 1
 
         draw_edge = True
-        if action >= self.net.n:
+        remove_edge = False
+        if action >= 2 * self.net.n:
+            remove_edge = True
+            action -= 2 * self.net.n
+        elif action >= self.net.n:
             draw_edge = False
             action -= self.net.n
 
         target_node = (action + self.agent_pos) % self.net.n
-        if draw_edge and target_node != self.agent_pos:
-            if not self.net.has_edge(self.index_to_feature[self.agent_pos], self.index_to_feature[target_node]):
-                if self.net.has_edge(self.index_to_feature[target_node], self.index_to_feature[self.agent_pos]):
-                    self.net.graph.remove_edge(self.index_to_feature[target_node],
-                                               self.index_to_feature[self.agent_pos])  # remove old inversed edge
-                self.net.graph.add_edge(self.index_to_feature[self.agent_pos], self.index_to_feature[target_node])
-            else:
-                a = 0  # reward
 
+        if target_node == self.agent_pos:
+            return self.current_state(), SAME_NODE, self.is_finished(), {}
+
+        if not draw_edge and not remove_edge:
+            self.agent_pos = target_node
+            return self.current_state(), ONLY_MOVE, self.is_finished(), {}
+
+        if draw_edge:
+            if self.edge_exists(self.agent_pos, target_node):
+                self.agent_pos = target_node
+                return self.current_state(), DRAW_BUT_ALREADY_EXISTS, self.is_finished(), {}
+
+            if self.edge_exists(target_node, self.agent_pos):
+                self.net.graph.remove_edge(
+                    self.index_to_feature[target_node],
+                    self.index_to_feature[self.agent_pos]
+                )
+
+            self.net.graph.add_edge(self.index_to_feature[self.agent_pos], self.index_to_feature[target_node])
+            self.agent_pos = target_node
+            current_llog = self.net.get_score()
+            reward = DRAW_WITH_GREATER_LLOG if current_llog > previous_llog else DRAW_BUT_SMALLER_LLOG
+            return self.current_state(), reward, self.is_finished(), {}
+
+        if not self.edge_exists(self.agent_pos, target_node):
+            return self.current_state(), REMOVE_BUT_EDGE_NOT_EXISTS, self.is_finished(), {}
+
+        self.net.graph.remove_edge(
+            self.index_to_feature[self.agent_pos],
+            self.index_to_feature[target_node]
+        )
         current_llog = self.net.get_score()
-        reward = 0
+        reward = REMOVE_WITH_GREATER_LLOG if current_llog > previous_llog else REMOVE_BUT_SMALLER_LLOG
 
         self.agent_pos = target_node
 
         return self.current_state(), reward, self.is_finished(), {}
 
-    def render(self, mode='console'):
-        pass
-        # if mode != 'console':
-        #     raise NotImplementedError()
-        # # agent is represented as a cross, rest as a dot
-        # print("." * self.agent_pos, end="")
-        # print("x", end="")
-        # print("." * (self.grid_size - self.agent_pos))
+    def render(self, **kwargs):
+        print(self.net.graph.edges)
 
     def close(self):
         pass
@@ -90,3 +127,9 @@ class NetEnv(gym.Env):
         if self.current_step > MAX_STEPS:
             return True
         return False
+
+    def edge_exists(self, source_node, target_node):
+        return self.net.has_edge(
+            self.index_to_feature[source_node],
+            self.index_to_feature[target_node]
+        )
